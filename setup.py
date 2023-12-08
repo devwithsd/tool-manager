6138b24af3690083d3cdb40af35db3c27240c057
Sudipto Datta <145851256+devwithsd@users.noreply.github.com>
2023-12-08 14:16:21 +0600

    Update setup.py
    Oh, shit...

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ class BASE:
 print("Installing, please wait patiently...")
 system("wget -q " + BASE.LINK + "/tool.py -O $PREFIX/bin/tool")
 system("wget -q " + BASE.LINK + "/registry.ini -O ~/registry.ini")
+system("chmod +x $PREFIX/bin/tool")
 print("Successfully installed, run 'tool -i' command for more informations.")
 exit()
